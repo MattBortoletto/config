c60ef7651444b9575691847e2e80880705dc6ec8
Matteo Bortoletto <56031078+MattBortoletto@users.noreply.github.com>
2026-06-23 11:16:29 +0200

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,13 +1,19 @@
-local wezterm = require 'wezterm'
-
+local wezterm = require("wezterm")
+local act = wezterm.action
 local config = wezterm.config_builder()
 
 config.quit_when_all_windows_are_closed = false
+config.audible_bell = "Disabled"
 config.check_for_updates = true
 config.hide_tab_bar_if_only_one_tab = true
+config.font_size = 12
+config.use_fancy_tab_bar = false
+config.enable_kitty_keyboard = true
 
-config.color_scheme = "tokyonight_night"
-config.font = wezterm.font("JetBrains Mono")
-config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+config.keys = {
+	{ key = "LeftArrow", mods = "OPT", action = act.SendString("\x1bb") },
+	{ key = "RightArrow", mods = "OPT", action = act.SendString("\x1bf") },
+	{ key = "Enter", mods = "SHIFT", action = act.SendString("\x1b[13;2u") },
+}
 
 return config
